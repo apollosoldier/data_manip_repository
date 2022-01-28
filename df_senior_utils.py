89a825e3568f154bb5d5b6c52b5c6bf8349239db
Mohamed Traoré <39139566+apollosoldier@users.noreply.github.com>
2022-01-28 20:55:41 +0100

    Update df_senior_utils.py

--- df_senior_utils.py
+++ df_senior_utils.py
@@ -1,5 +1,41 @@
-%pylab inline
+import os
+import gc
+import sys
+
+import random
+import numpy as np
 import pandas as pd
+import seaborn as sns
+
+from tqdm.notebook import *
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import warnings
+tqdm_notebook().pandas()
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 500)
+warnings.simplefilter("ignore")
+
+## BEG Models ##
+import lightgbm as lgb
+from catboost import CatBoostRegressor
+from catboost import CatBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.mixture import GaussianMixture, BayesianGaussianMixture
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+
+## END Models ##
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+
+
+my_models = [GradientBoostingClassifier(),
+             lgbm(), CatBoostClassifier(), XGBClassifier(),
+             RandomForestClassifier(), GaussianMixture()]
 
 
 def check_na_ratio(df):
